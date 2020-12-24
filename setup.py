0eaa8772f6c79438b211b9480a3b88b0d36cd13c
Andreas Rjasanow <andrjas@gmail.com>
2020-12-24 19:29:00 +0100

    added extras_requires for the tested databases

--- setup.py
+++ setup.py
@@ -14,6 +14,18 @@ def get_version(rel_path):
 long_description = Path("README.md").read_text()
 
 requirements = Path("requirements.txt").read_text().splitlines()
+postgres_requirements = (
+    Path("dependencies/requirements-postgres.txt").read_text().splitlines()
+)
+oracle_requirements = (
+    Path("dependencies/requirements-oracle.txt").read_text().splitlines()
+)
+mysql_requirements = (
+    Path("dependencies/requirements-mysql.txt").read_text().splitlines()
+)
+mssql_requirements = (
+    Path("dependencies/requirements-mssql.txt").read_text().splitlines()
+)
 
 setup(
     name="data_check",
@@ -25,5 +37,11 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=requirements,
+    extras_require={
+        "postgres": postgres_requirements,
+        "oracle": oracle_requirements,
+        "mysql": mysql_requirements,
+        "mssql": mssql_requirements,
+    },
     python_requires=">=3.6",
 )
