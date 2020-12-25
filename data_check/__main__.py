96a32e2ca207d5fc81fe68820b9c1bfe9d78c458
Andreas Rjasanow <andrjas@gmail.com>
2020-12-25 18:53:07 +0100

    rewritten with click instead of argsparse

--- data_check/__main__.py
+++ data_check/__main__.py
@@ -1,57 +1,9 @@
-import argparse
+import click
 from pathlib import Path
 import sys
 
 from data_check.data_check import DataCheck
 from data_check import __version__ as data_check_version
-
-
-def parse_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description="data_check")
-
-    parser.add_argument("-c", "--connection", type=str, help="connection to use")
-    parser.add_argument(
-        "-n",
-        "--workers",
-        type=int,
-        default=4,
-        help="parallel workers to run queries (default: 4)",
-    )
-    parser.add_argument(
-        "--print", action="store_true", dest="print_failed", help="print failed results"
-    )
-    parser.add_argument(
-        "--gen",
-        "--generate",
-        dest="generate_expectations",
-        action="store_true",
-        help="generate expectation files if they don't exist",
-    )
-    parser.add_argument(
-        "--config",
-        type=str,
-        default="data_check.yml",
-        help="config file to use (default: data_check.yml)",
-    )
-    parser.add_argument(
-        "--ping",
-        action="store_true",
-        help="tries to connect to the database",
-    )
-    parser.add_argument(
-        "--version",
-        action="store_true",
-        help="print version and exit",
-    )
-    parser.add_argument(
-        "files",
-        metavar="files",
-        type=str,
-        nargs="*",
-        default=["checks"],
-        help="list of checks files or folders",
-    )
-    return parser.parse_args()
 
 
 def select_connection(connection, config) -> str:
@@ -66,29 +18,64 @@ def select_connection(connection, config) -> str:
     return config.get("connections", {}).get(connection)
 
 
-def main():
-    args = parse_args()
-    config = DataCheck.read_config(Path(args.config))
+@click.command()
+@click.option("--connection", "-c", type=str, help="connection to use")
+@click.option(
+    "--workers",
+    "-n",
+    type=int,
+    default=4,
+    help="parallel workers to run queries (default: 4)",
+)
+@click.option("--print", "print_failed", is_flag=True, help="print failed results")
+@click.option(
+    "--gen",
+    "--generate",
+    "generate_expectations",
+    is_flag=True,
+    help="generate expectation files if they don't exist",
+)
+@click.option(
+    "--config",
+    type=str,
+    default="data_check.yml",
+    help="config file to use (default: data_check.yml)",
+)
+@click.option("--ping", is_flag=True, help="tries to connect to the database")
+@click.version_option(version=data_check_version)
+@click.argument("files", nargs=-1, type=click.Path())
+def main(
+    connection=None,
+    workers=4,
+    print_failed=False,
+    generate_expectations=False,
+    config="data_check.yml",
+    ping=False,
+    files=[],
+):
+    """ FILES: list of checks files or folders"""
 
-    selected_connection = select_connection(args.connection, config)
+    config = DataCheck.read_config(Path(config))
+    selected_connection = select_connection(connection, config)
 
-    dc = DataCheck(connection=selected_connection, workers=args.workers)
-    path_list = [Path(f) for f in args.files]
+    dc = DataCheck(connection=selected_connection, workers=workers)
 
-    if args.version:
-        print(data_check_version)
-    elif args.generate_expectations:
+    if not files:
+        files = ["checks"]  # use "checks" if nothing is given
+    path_list = [Path(f) for f in files]
+
+    if generate_expectations:
         dc.generate_expectations(path_list)
-    elif args.ping:
+    elif ping:
         test = dc.test_connection()
         if test:
-            print(f"connecting succeeded")
+            click.echo("connecting succeeded")
             sys.exit(0)
         else:
-            print(f"connecting failed")
+            click.echo("connecting failed")
             sys.exit(1)
     else:
-        result = dc.run(path_list, print_failed=args.print_failed)
+        result = dc.run(path_list, print_failed=print_failed)
         if not result:
             sys.exit(1)
 
